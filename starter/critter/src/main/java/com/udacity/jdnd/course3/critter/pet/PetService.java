073c4613566a98020d23073cc3b9ac02e26f46dc
Akhil <mattaakhil@gmail.com>
2025-04-23 09:04:32 +0530

    added @transactional to service classes

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/PetService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/PetService.java
@@ -4,10 +4,12 @@ import com.udacity.jdnd.course3.critter.user.Customer;
 import com.udacity.jdnd.course3.critter.user.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class PetService {
 
     @Autowired
